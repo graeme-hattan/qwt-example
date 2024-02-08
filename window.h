8ddd5448535dbd880a3176bca640f5bf585c2e64
Bernd Porr <mail@berndporr.me.uk>
2024-02-08 11:43:26 +0000

    Now with cpp timer

--- window.h
+++ window.h
@@ -8,38 +8,58 @@
 #include <QBoxLayout>
 #include <QPushButton>
 
+#include "CppTimer.h"
+
 // class definition 'Window'
 class Window : public QWidget
 {
-	// must include the Q_OBJECT macro for for the Qt signals/slots framework to work with this class
-	Q_OBJECT
+    // must include the Q_OBJECT macro for for the Qt signals/slots framework to work with this class
+    Q_OBJECT
 
+
+    class FakeSensor : public CppTimer {
+	static constexpr double gain = 7.5;
+    public:
+	Window* window = nullptr;
+	int count = 0;
+	void timerEvent() {
+	    double inVal = gain * sin( M_PI * count / 50.0 );
+	    window->hasData(inVal);
+	    ++count;
+	}
+    };
+
+    
 public:
-	Window(); // default constructor - called when a Window is declared without arguments
+    Window(); // default constructor - called when a Window is declared without arguments
 
-	void timerEvent( QTimerEvent * );
+    void timerEvent( QTimerEvent * );
 
 // internal variables for the window class
 private:
-	static constexpr int plotDataSize = 100;
-	static constexpr double gain = 7.5;
+    static constexpr int plotDataSize = 100;
 
-	QPushButton  *button;
-	QwtThermo    *thermo;
-	QwtPlot      *plot;
-	QwtPlotCurve *curve;
+    QPushButton  *button;
+    QwtThermo    *thermo;
+    QwtPlot      *plot;
+    QwtPlotCurve *curve;
 
-	// layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
-	QVBoxLayout  *vLayout;  // vertical layout
-	QHBoxLayout  *hLayout;  // horizontal layout
+    
 
-	// data arrays for the plot
-	double xData[plotDataSize];
-	double yData[plotDataSize];
+    // layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
+    QVBoxLayout  *vLayout;  // vertical layout
+    QHBoxLayout  *hLayout;  // horizontal layout
 
-	long count = 0;
+    FakeSensor fakeSensor;
 
-	void reset();
+    // data arrays for the plot
+    double xData[plotDataSize];
+    double yData[plotDataSize];
+
+    long count = 0;
+
+    void reset();
+    void hasData(double v);
 };
 
 #endif // WINDOW_H
